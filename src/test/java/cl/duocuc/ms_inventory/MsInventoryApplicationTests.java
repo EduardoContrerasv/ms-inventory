c8b7d6795cf2e51f8249baf2edd53330ff990a24
EduardoCV <edu.contrerasv@duocuc.cl>
2026-05-19 16:51:45 -0400

    Creacion ms-inventory y configuracion de properties

---
+++ src/test/java/cl/duocuc/ms_inventory/MsInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duocuc.ms_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
